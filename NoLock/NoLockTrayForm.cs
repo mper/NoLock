328b244d9882b0b114cf47b2a5ca3d926aec0849
Mperija <mperija@yahoo.com>
2016-02-23 09:34:33 +0100

    + Add NoLockWinApi, uses windows api to prevent locking. - Remove MouseMovement, mouse move simulation does not work using remote desktop. * Update NoLockTrayForm, using NoLockWinApi, cleanup.

--- NoLock/NoLockTrayForm.cs
+++ NoLock/NoLockTrayForm.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Drawing;
 using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NoLock
 {
     class NoLockTrayForm : Form
     {
-        private MouseMove mouse;
         private NotifyIcon trayIcon;
         private ContextMenu trayMenu;
         private MenuItem infoMenuItem;
-        private Timer trayMenuDisplayTimer;
 
         public NoLockTrayForm(int? interval = null)
         {
             trayMenu = new ContextMenu();
-            trayMenu.Popup += TrayMenu_Popup;
-            trayMenu.Collapse += TrayMenu_Collapse;
-            infoMenuItem = new MenuItem() { Enabled = false };
+            infoMenuItem = new MenuItem() { Enabled = false, Text = "NoLock" };
             trayMenu.MenuItems.Add(infoMenuItem);
             trayMenu.MenuItems.Add("Exit", OnExit);
 
@@ -28,21 +23,9 @@ namespace NoLock
             trayIcon.Icon = new Icon(Icon.ExtractAssociatedIcon(Application.ExecutablePath), 40, 40);
             trayIcon.ContextMenu = trayMenu;
             trayIcon.MouseClick += trayIcon_MouseClick;
-            
-            mouse = new MouseMove(interval);
-            infoMenuItem.Text = string.Concat("Interval: ", (mouse.Interval / 1000d).ToString(), "s");
-            mouse.MouseMoved += mouse_MouseMoved;
 
-            trayMenuDisplayTimer = new Timer();
-            trayMenuDisplayTimer.Interval = 1000;
-            trayMenuDisplayTimer.Tick += trayMenuDisplayTimer_Tick;
-        }
 
-        private void trayIcon_MouseClick(object sender, MouseEventArgs e)
-        {
-            if(e.Button == MouseButtons.Left)
-                typeof(NotifyIcon).GetMethod("ShowContextMenu",
-                    BindingFlags.Instance | BindingFlags.NonPublic).Invoke(trayIcon, null);
+            NoLockWinApi.SetExecutionState();
         }
 
         protected override void OnLoad(EventArgs e)
@@ -61,35 +44,19 @@ namespace NoLock
             base.Dispose(disposing);
         }
 
-        private void TrayMenu_Popup(object sender, EventArgs e)
+        /// <summary>
+        /// Enable tray context menu on left mouse click
+        /// </summary>
+        private void trayIcon_MouseClick(object sender, MouseEventArgs e)
         {
-            trayMenuDisplayTimer_Tick(null, null);
-            trayMenuDisplayTimer.Start();
-        }
-
-        private void TrayMenu_Collapse(object sender, EventArgs e)
-        {
-            trayMenuDisplayTimer.Stop();
-        }
-
-        private void trayMenuDisplayTimer_Tick(object sender, EventArgs e)
-        {
-            int mouseMoveCountdown = mouse.Interval / 1000 - 
-                (int)(DateTime.Now - mouse.LastMouseMoved).TotalSeconds;
-            infoMenuItem.Text = string.Format("Interval: {0}s ({1}){2}",
-                (mouse.Interval / 1000d).ToString(), mouseMoveCountdown,
-                mouseMoveCountdown == mouse.Interval / 1000 ? "*" : "");
+            if (e.Button == MouseButtons.Left)
+                typeof(NotifyIcon).GetMethod("ShowContextMenu",
+                    BindingFlags.Instance | BindingFlags.NonPublic).Invoke(trayIcon, null);
         }
 
         private void OnExit(object sender, EventArgs e)
         {
-            mouse.Dispose();
             Close();
-        }
-
-        private void mouse_MouseMoved(object sender, EventArgs e)
-        {
-            // do something
         }
     }
 }
---
+++ NoLock/NoLockWinApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NoLock
+{
+    static class NoLockWinApi
+    {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT   = 0x00000004,
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        /// <summary>
+        /// Uses Win API to prevent lock or sleep
+        /// </summary>
+        public static void SetExecutionState()
+        {
+            if (SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                | EXECUTION_STATE.ES_SYSTEM_REQUIRED
+                | EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0) //Away mode for Windows >= Vista
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED); //Windows < Vista, forget away mode
+        }
+    }
+}
